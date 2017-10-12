890e457289db8004528f68d2b646bd88ae0eed7d
YangKa <m15347067008@163.com>
2017-10-12 18:08:45 +0800

    UIImagePickerController 视频录制 连拍

--- MovieStudyDemo/ViewController.m
+++ MovieStudyDemo/ViewController.m
@@ -12,9 +12,10 @@
 //AVCaptureSession
 #import <AVFoundation/AVFoundation.h>
 
-@interface ViewController ()<UINavigationControllerDelegate, UIImagePickerControllerDelegate>{
+@interface ViewController ()<UINavigationControllerDelegate, UIImagePickerControllerDelegate
+                            ,AVCaptureFileOutputRecordingDelegate>{
     AVCaptureSession *_captureSession;
-    
+                                UIImagePickerController *picker;
 }
 
 @end
@@ -24,51 +25,100 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+}
+
+- (void)viewDidAppear:(BOOL)animated{
+    [super viewDidAppear:animated];
+    [self captureMovieByImagePicker];
 }
 
 //UIKit
 - (void)captureMovieByImagePicker{
     if ([UIImagePickerController isSourceTypeAvailable:UIImagePickerControllerSourceTypeCamera]) {
         
-        UIImagePickerController *picker = [[UIImagePickerController alloc] init];
+        
+        picker = [[UIImagePickerController alloc] init];
         
         picker.sourceType = UIImagePickerControllerSourceTypeCamera;//相机
+        
         picker.mediaTypes = @[(NSString*)kUTTypeMovie];//默认只有拍照界面kUTTypeImage
-        if ([UIImagePickerController isCameraDeviceAvailable:UIImagePickerControllerCameraDeviceFront]){
-            picker.cameraDevice = UIImagePickerControllerCameraDeviceFront;//前后摄像头
-        }
-        
+//        if ([UIImagePickerController isCameraDeviceAvailable:UIImagePickerControllerCameraDeviceFront]){
+//            picker.cameraDevice = UIImagePickerControllerCameraDeviceFront;//前后摄像头
+//        }
+
         picker.videoQuality = UIImagePickerControllerQualityTypeHigh;
-        
-        picker.showsCameraControls = NO;
-        UIView *cameraOverlay = [[UIView alloc] initWithFrame:CGRectMake(0, picker.view.frame.size.height-100, picker.view.frame.size.width, 100)];
-        cameraOverlay.backgroundColor = [UIColor redColor];
+	
+        /*
+        //自定义overlay
+        picker.showsCameraControls = NO;//YES 拍完照后需要在代理方法里dismiss picker。自定义可以mutilate capture
+        UIView *cameraOverlay = [[UIView alloc] initWithFrame:picker.view.bounds];
+        cameraOverlay.backgroundColor = [UIColor clearColor];
         picker.cameraOverlayView = cameraOverlay;
         
-        UIButton *button = [[UIButton alloc] initWithFrame:CGRectMake(25, 25, 50, 50)];
-        button.backgroundColor = [UIColor blueColor];
-        [button addTarget:picker action:@selector(startVideoCapture) forControlEvents:UIControlEventTouchUpInside];
-        [cameraOverlay addSubview:button];
+        UIButton *cancelButton = [[UIButton alloc] initWithFrame:CGRectMake(cameraOverlay.frame.size.width - 75, 25, 50, 50)];
+        cancelButton.backgroundColor = [UIColor blueColor];
+        [cancelButton setTitle:@"取消" forState:UIControlStateNormal];
+        [cancelButton setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
+        [cancelButton addTarget:self action:@selector(dismiss) forControlEvents:UIControlEventTouchUpInside];
+        [cameraOverlay addSubview:cancelButton];
         
-        UIButton *button1 = [[UIButton alloc] initWithFrame:CGRectMake(cameraOverlay.frame.size.width - 75, 25, 50, 50)];
-        button1.backgroundColor = [UIColor blueColor];
-        [button1 addTarget:picker action:@selector(stopVideoCapture) forControlEvents:UIControlEventTouchUpInside];
-        [cameraOverlay addSubview:button1];
+        UIView *bottomView = [[UIView alloc] initWithFrame:CGRectMake(0, cameraOverlay.frame.size.height- 100, cameraOverlay.frame.size.width, 100)];
+        bottomView.backgroundColor = [UIColor clearColor];
+        [cameraOverlay addSubview:bottomView];
+
+        UIButton *beginButton = [[UIButton alloc] initWithFrame:CGRectMake(25, 25, 100, 50)];
+        beginButton.backgroundColor = [UIColor blueColor];
+        [beginButton setTitle:@"开始录制" forState:UIControlStateNormal];
+        [beginButton setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
+        [beginButton addTarget:picker action:@selector(startVideoCapture) forControlEvents:UIControlEventTouchUpInside];
+        [bottomView addSubview:beginButton];
+        
+        UIButton *captureButton = [[UIButton alloc] initWithFrame:CGRectMake(0, 0, 50, 50)];
+        captureButton.center = CGPointMake(bottomView.frame.size.width/2, bottomView.frame.size.height/2);
+        captureButton.backgroundColor = [UIColor redColor];
+        captureButton.layer.cornerRadius = captureButton.frame.size.width/2;
+        captureButton.layer.masksToBounds = YES;
+        [captureButton addTarget:picker action:@selector(takePicture) forControlEvents:UIControlEventTouchUpInside];
+        [bottomView addSubview:captureButton];
+
+        UIButton *endButton = [[UIButton alloc] initWithFrame:CGRectMake(cameraOverlay.frame.size.width - 125, 25, 100, 50)];
+        endButton.backgroundColor = [UIColor blueColor];
+        [endButton setTitle:@"结束录制" forState:UIControlStateNormal];
+        [endButton setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
+        [endButton addTarget:picker action:@selector(stopVideoCapture) forControlEvents:UIControlEventTouchUpInside];
+        [bottomView addSubview:endButton];
+        */
         
         picker.delegate = self;
+        
+
         [self presentViewController:picker animated:YES completion:nil];
     }
 
 }
 
+- (void)dismiss{
+    [picker dismissViewControllerAnimated:YES completion:nil];
+    picker = nil;
+}
+
+#pragma mark UIImagePickerControllerDelegate
 - (void)imagePickerControllerDidCancel:(UIImagePickerController *)picker{
     
 }
 
 - (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary<NSString *,id> *)info{
-    
+    NSLog(@"%@", info);
+    if ([info[UIImagePickerControllerMediaType] isEqualToString:(NSString*)kUTTypeImage]) {
+        NSLog(@"image=%@", info[UIImagePickerControllerOriginalImage]);
+    }
+    if ([info[UIImagePickerControllerMediaType] isEqualToString:(NSString*)kUTTypeMovie]) {
+        NSLog(@"movie Path=%@", info[UIImagePickerControllerMediaURL]);
+    }
 }
 
+#pragma mark
 //AVFoundation 中的AVCaptureSession
 - (void)captureMovieByAVFoundation{
     _captureSession = [[AVCaptureSession alloc] init];
@@ -115,7 +165,25 @@
     }
     
     //视频防抖
+    //防抖不是在设备上配置的，而是在AVCaptureConnection上配置
+    AVCaptureConnection *connection = [[AVCaptureConnection alloc] init];
+    
+    AVCaptureVideoStabilizationMode stabilizationMode = AVCaptureVideoStabilizationModeCinematic;
+    if ([videoDevice.activeFormat isVideoStabilizationModeSupported:stabilizationMode]){
+        [connection setPreferredVideoStabilizationMode:stabilizationMode];
+    }
+    
+    //视频 HDR (高动态范围图像)
+    videoDevice.automaticallyAdjustsVideoHDREnabled = YES;
+    
+    //开始录制
+//    [fileOutput startRecordingToOutputFileURL:nil recordingDelegate:self];
+//    [fileOutput stopRecording];
+//    [fileOutput pauseRecording];
+//    [fileOutput resumeRecording];
 }
+
+
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
